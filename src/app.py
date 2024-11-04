0fa4cd2ec96c9c7210529eeab7fa4ea8b3850047
Shankar <shankarselvaraj24@gmail.com>
2024-11-04 21:58:06 +0530

    it works

--- src/app.py
+++ src/app.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import dlib
 from PIL import Image
 import pillow_heif
-import io
 
 detector = dlib.get_frontal_face_detector()
 
@@ -54,6 +53,54 @@ def detect_face(image_np):
     face = image_np[y : y + h, x : x + w]
     face_resized = cv2.resize(face, (TARGET_FACE_WIDTH, TARGET_FACE_HEIGHT))
     return face_resized
+
+
+def load_image(image_path):
+    """Load an image from a given path and convert it to RGB format."""
+    image = cv2.imread(image_path)
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB) if image is not None else None
+
+
+def attach_hair_and_ears(
+    face_image, hair_image_path, left_ear_image_path, right_ear_image_path
+):
+    """Attach hair and ears to the face image without modifying the face image size."""
+    hair_image = load_image(hair_image_path)
+    left_ear_image = load_image(left_ear_image_path)
+    right_ear_image = load_image(right_ear_image_path)
+
+    combined_height = face_image.shape[0] + hair_image.shape[0]
+    combined_width = (
+        face_image.shape[1] + left_ear_image.shape[1] + right_ear_image.shape[1]
+    )
+
+    combined_image = np.ones((combined_height, combined_width, 3), dtype=np.uint8) * 255
+
+    hair_x_offset = left_ear_image.shape[1]
+
+    combined_image[
+        : hair_image.shape[0], hair_x_offset : hair_x_offset + hair_image.shape[1]
+    ] = hair_image
+
+    combined_image[
+        hair_image.shape[0] :, hair_x_offset : hair_x_offset + face_image.shape[1]
+    ] = face_image
+
+    if left_ear_image is not None:
+        ear_height = left_ear_image.shape[0]
+        ear_width = left_ear_image.shape[1]
+        combined_image[
+            hair_image.shape[0] : ear_height + hair_image.shape[0], :ear_width
+        ] = left_ear_image
+
+    if right_ear_image is not None:
+        ear_height = right_ear_image.shape[0]
+        ear_width = right_ear_image.shape[1]
+        combined_image[
+            hair_image.shape[0] : ear_height + hair_image.shape[0], -ear_width:
+        ] = right_ear_image
+
+    return combined_image
 
 
 def get_stick_figure_image_path(stick_figures_folder, manual_image_path=None):
@@ -106,10 +153,18 @@ if __name__ == "__main__":
     stick_figures_folder = "images/stick_figures"
     user_image_path = input("Please enter the path to your image: ")
 
+    hair_image_path = "images/stick_figures/hair.png"
+    left_ear_image_path = "images/stick_figures/left_ear.png"
+    right_ear_image_path = "images/stick_figures/right_ear.png"
+
     try:
         image_np = convert_to_jpeg(user_image_path)
 
         face_image = detect_face(image_np)
+
+        face_with_hair_and_ears = attach_hair_and_ears(
+            face_image, hair_image_path, left_ear_image_path, right_ear_image_path
+        )
 
         stick_figure_image_path = get_stick_figure_image_path(
             stick_figures_folder,
@@ -119,7 +174,9 @@ if __name__ == "__main__":
             cv2.imread(stick_figure_image_path), cv2.COLOR_BGR2RGB
         )
 
-        final_image = combine_face_with_stick_figure(face_image, stick_figure_image)
+        final_image = combine_face_with_stick_figure(
+            face_with_hair_and_ears, stick_figure_image
+        )
 
         plt.figure(figsize=(5, 8))
         plt.imshow(final_image)
